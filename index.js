d245ec812739ecebb2300869d93c1fb1b2e44ea6
zain ul rab <zayn.rab604@gmail.com>
2023-06-18 06:59:55 +0500

    Second commit

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    //banner owl carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1
+    }); 
+    
+    //top-sales
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+    
+}); 
+
+
+
+
+
+  
+  
